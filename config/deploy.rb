9ae395f796c72b871ddd3ca7f32081d3036bb21d
katawo <sora.kataoka@gmail.com>
2018-01-05 15:51:24 +0900

    test3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-lock '3.4.0'
+lock '3.10.1'
 
 set :application, 'infumel'
 set :repo_url, 'https://github.com/katao/infumel.git'
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,12 +1,15 @@
 set :stage, :development
-role :app, %w{root@153.126.188.66}
-role :web, %w{root@153.126.188.66}
-role :db, %w{root@153.126.188.66}
+role :app, %w{admin@153.126.188.66}
+role :web, %w{admin@153.126.188.66}
+role :db, %w{admin@153.126.188.66}
 
 server '153.126.188.66',
-user: 'root',
+user: 'admin',
 roles: %w{web app db},
 ssh_options: {
- auth_methods: %w(password),
- password: '*****'
+  # ローカル環境のユーザーの秘密鍵ファイルを設定する
+  keys: %w(/Users/kataokadaichi/Dropbox/key/個人鍵/id_rsa),
+  forward_agent: true,
+ #auth_methods: %w(password),
+ #password: 'daithi0808'
 }
